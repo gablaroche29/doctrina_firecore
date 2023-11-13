48c6c918cb845b08ef5efd44874daf434ad8b352
gabriellaroche <gabriel.laroche29@gmail.com>
2023-11-13 15:56:52 -0500

    refactor: extract world class so it can work alone, the heavenlygame class is not just a big class that work without variables like crazy

--- src/heavenly/HeavenlyGame.java
+++ src/heavenly/HeavenlyGame.java
@@ -2,23 +2,14 @@ package heavenly;
 
 import doctrina.*;
 import doctrina.Canvas;
-import heavenly.ennemy.Ennemies;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HeavenlyGame extends Game {
 
     private GamePad gamePad;
     private Player player;
-
-    private Pillar pillar;
-
-    private List<MovableEntity> collidableEntities;
     private World world;
-    private List<StaticEntity> renderingEntities;
-    private Ennemies ennemies;
 
     @Override
     protected void initialize() {
@@ -26,21 +17,7 @@ public class HeavenlyGame extends Game {
         gamePad = new GamePad();
         player = new Player(gamePad, 864, 2368);
         world = new World(player);
-
         camera = new Camera(player, 800, 600);
-
-        ennemies = new Ennemies(world, player);
-
-
-        collidableEntities = new ArrayList<>();
-        collidableEntities.add(player);
-        collidableEntities.addAll(ennemies.getEnnemies());
-
-//        renderingEntities = new ArrayList<>();
-
-//        pillar = new Pillar();
-
-
 
         RenderingEngine.getInstance().getScreen().fullscreen();
     }
@@ -52,28 +29,14 @@ public class HeavenlyGame extends Game {
             stop();
         }
         player.update();
-
-//        for (StaticEntity entity : renderingEntities) {
-//            entity.setRender(entity.isInCameraField(camera));
-//        }
-
-        world.update(collidableEntities);
-        ennemies.update();
+        world.update();
     }
 
 
     @Override
     protected void draw(Canvas canvas) {
         world.draw(canvas, camera);
-//        for (StaticEntity entity : renderingEntities) {
-//            if (entity.isRender()) {
-//                entity.draw(canvas, camera);
-//            }
-//        }
         player.draw(canvas, camera);
         canvas.drawString("FPS " + GameTime.getCurrentFps(), 20, 20, Color.WHITE);
-
-        ennemies.draw(canvas, camera);
-//        pillar.draw(canvas, camera);
     }
 }
--- src/heavenly/World.java
+++ src/heavenly/World.java
@@ -2,56 +2,52 @@ package heavenly;
 
 import doctrina.*;
 import doctrina.Canvas;
+import heavenly.ennemy.Ennemies;
+import heavenly.sounds.Music;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 public class World extends StaticEntity {
 
     private static final String MAP_PATH = "image/background/heavenly/Map.png";
-    private static final String PROP_PATH = "image/props/Props.png";
     private static final String COLLISIONS_PATH = "resources/collisions/collision_heavenly.txt";
     private Image background;
 
-    private int limitLeft, limitDown, limitRight, limitUp;
-
-    private CollisionRepository collisionRepository;
-    private Player player;
+    private final CollisionRepository collisionRepository;
+    private List<MovableEntity> collidableEntities;
+    private final Player player;
+    private final Ennemies ennemies;
 
     public World(Player player) {
         setDimension(3200, 3200);
         teleport(0, 0);
         load();
-        limitLeft = 640;
-        limitDown = 2752;
         collisionRepository = new CollisionRepository(COLLISIONS_PATH, 100, 100, 32, 833);
-
         this.player = player;
+
+        ennemies = new Ennemies(this, player);
+
+        initializeCollidableEntities();
+        playBackgroundMusic();
     }
 
-    public int getLimitLeft() {
-        return limitLeft;
-    }
-
-    public int getLimitDown() {
-        return limitDown;
-    }
-
-    private void updateCollisionWorld(Collection<MovableEntity> entities) {
-        for (Blockade blockade : collisionRepository.getCollisions()) {
-            blockade.update(entities);
-        }
-    }
-
-    public void update(Collection<MovableEntity> entities) {
-        updateCollisionWorld(entities);
+    public void update() {
+        ennemies.update();
+        updateCollisionWorld();
     }
 
     @Override
     public void draw(Canvas canvas, Camera camera) {
         canvas.drawImage(background, x - camera.getX(), y - camera.getY());
+        for (Ia ia : ennemies.getEnnemies()) {
+            ia.draw(canvas, camera);
+        }
 
         if (GameConfig.isDebugEnabled()) {
             for (Blockade blockade : collisionRepository.getCollisions()) {
@@ -59,13 +55,31 @@ public class World extends StaticEntity {
             }
         }
     }
+    
+    private void initializeCollidableEntities() {
+        collidableEntities = new ArrayList<>();
+        collidableEntities.add(player);
+        collidableEntities.addAll(ennemies.getEnnemies());
+    }
+
+    private void updateCollisionWorld() {
+        for (Blockade blockade : collisionRepository.getCollisions()) {
+            blockade.update(collidableEntities);
+        }
+    }
 
     private void load() {
         try {
             background = ImageIO.read(
-                    this.getClass().getClassLoader().getResourceAsStream(MAP_PATH));
+                    Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream(MAP_PATH)));
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
+        }
+    }
+
+    private void playBackgroundMusic() {
+        if (!GameConfig.isDebugEnabled()) {
+            Music.BG_AMBIENT.play();
         }
     }
 }
