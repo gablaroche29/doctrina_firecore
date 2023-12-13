b2e6b9baad7b029f27e5d773271a6569234c17c5
gabriellaroche <gabriel.laroche29@gmail.com>
2023-12-12 20:48:45 -0500

    refactor: change the ui event chest

--- src/doctrina/Ui.java
+++ src/doctrina/Ui.java
@@ -7,17 +7,13 @@ import java.awt.*;
 import java.util.Arrays;
 
 public class Ui {
-
-    private final int x = 0;
-    private final int y = 0;
-    private final int width = 800;
-    private final int height = 600;
     private final Image[] healthBar;
     private final String healthBarPath = "image/ui/health_bar.png";
     private final FontLoader fontLoader;
 
     private static boolean openChest;
     private static String[] texts;
+    private static String[] eventText;
 
     private final Player player;
 
@@ -38,10 +34,19 @@ public class Ui {
     }
 
     private void drawDialogue(Canvas canvas) {
-        canvas.drawRoundRectangle(150, 32, 500, 200, 35, 35, Color.BLACK);
+        canvas.drawRoundRectangle(150, 30, 500, 200, 35, 35, Color.BLACK);
         int startY = 70;
         for (String string : texts) {
             canvas.drawString(string, 180, startY, Color.WHITE, fontLoader.getFont());
+            startY += 40;
+        }
+    }
+
+    private void drawEvent(Canvas canvas) {
+        canvas.drawRoundRectangle(450, 400, 300, 150, 35, 35, Color.BLACK);
+        int startY = 440;
+        for (String string : eventText) {
+            canvas.drawString(string, 480, startY, Color.WHITE, fontLoader.getFont());
             startY += 40;
         }
     }
@@ -63,10 +68,15 @@ public class Ui {
         Ui.texts = texts.split("\n");
     }
 
+    private void setEventText(String text) {
+        eventText = text.split("\n");
+    }
+
     private void chestEvent(Canvas canvas) {
         String text = "Tu as ouvert un coffre!\nIl y avait deux lingots!";
         setTexts(text);
-        drawDialogue(canvas);
+        setEventText(text);
+        drawEvent(canvas);
         eventCooldown--;
         if (eventCooldown <= 0) {
             eventCooldown = 100;
